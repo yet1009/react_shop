3a49bbd4ea5c63e43a9c716eedd1fce087380cbc
yet1009 <yet1009@hanmail.net>
2023-08-15 21:14:34 +0900

    commits

---
+++ src/store/user/user.slice.js
@@ -0,0 +1,28 @@
+const {createSlice} = require("@reduxjs/toolkit");
+
+const initialState = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : { email: '', token: '', id: ''}
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        setUsers: (state, action) => {
+            state.email = action.payload.email;
+            state.token = action.payload.token;
+            state.id = action.payload.id;
+
+            localStorage.setItem('user', JSON.stringify(state))
+        },
+        removeUser: (state) => {
+            state.email = "";
+            state.token = "";
+            state.id = "";
+
+            localStorage.setItem('user', JSON.stringify(state))
+        },
+    }
+})
+
+export const { setUser, removeUser } = userSlice.actions;
+
+export default userSlice.reducer;
